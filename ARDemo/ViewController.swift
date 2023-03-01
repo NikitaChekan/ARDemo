bd67c4e08739c2b5f4963795c2568c4faa1afef4
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-01 14:54:10 +0300

    tap gesture

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -18,11 +18,14 @@ class ViewController: UIViewController {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
         let scene = SCNScene()
         
-        createFigures(in: scene)
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(boxTapped(touch:)))
+        self.sceneView.addGestureRecognizer(gestureRecognizer)
+        createBox(in: scene)
 //        createDimonRofl(in: scene)
         
         sceneView.scene = scene
@@ -45,25 +48,33 @@ class ViewController: UIViewController {
         sceneView.session.pause()
     }
     
-        private func createFigures(in scene: SCNScene) {
-            let array: [SCNGeometry] = [SCNPlane(), SCNSphere(), SCNBox(), SCNPyramid(), SCNTube(), SCNCone(), SCNTorus(), SCNCylinder(), SCNCapsule()]
-            var xCoordinate: Double = 1
-            sceneView.autoenablesDefaultLighting = true
+    @objc func boxTapped(touch: UITapGestureRecognizer) {
+        let sceneView = touch.view as! SCNView
+        let touchLocation = touch.location(in: sceneView)
+        
+        let touchResults = sceneView.hitTest(touchLocation)
+        
+        guard !touchResults.isEmpty, let node = touchResults.first?.node else { return }
+        let boxMaterial = SCNMaterial()
+        boxMaterial.diffuse.contents = UIColor.blue
+        boxMaterial.specular.contents = UIColor.red
+        
+        node.geometry?.materials[0] = boxMaterial
+    }
     
-            for geometryShape in array {
-                let node = SCNNode(geometry: geometryShape)
-                let material = SCNMaterial()
-                material.diffuse.contents = UIColor.red
+        private func createBox(in scene: SCNScene) {
     
-                node.geometry?.materials = [material]
-                node.scale = SCNVector3(0.1, 0.1, 0.1)
-    
-                node.position = SCNVector3(xCoordinate, 0, -1.5)
-                xCoordinate -= 0.2
-    
-                scene.rootNode.addChildNode(node)
-            }
-    
+            let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+            let boxMaterial = SCNMaterial()
+            
+            boxMaterial.diffuse.contents = UIColor.red
+            boxMaterial.specular.contents = UIColor.yellow
+            
+            let boxNode = SCNNode(geometry: box)
+            boxNode.geometry?.materials = [boxMaterial]
+            boxNode.position = SCNVector3(0.0, 0.0, -1.0)
+            
+            scene.rootNode.addChildNode(boxNode)
         }
     
     private func createDimonRofl(in scene: SCNScene) {
