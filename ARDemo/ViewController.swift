ed8fb439415254100ed9b544ae48ab81eade7632
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-01 16:01:37 +0300

    debug options , added reset and add buttons

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -10,8 +10,12 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    var session: ARSession {
+        return sceneView.session
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,14 +23,10 @@ class ViewController: UIViewController {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         sceneView.autoenablesDefaultLighting = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
         // Create a new scene
         let scene = SCNScene()
-        
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(boxTapped(touch:)))
-        self.sceneView.addGestureRecognizer(gestureRecognizer)
-        createBox(in: scene)
-//        createDimonRofl(in: scene)
         
         sceneView.scene = scene
     }
@@ -36,7 +36,7 @@ class ViewController: UIViewController {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -48,77 +48,36 @@ class ViewController: UIViewController {
         sceneView.session.pause()
     }
     
-    @objc func boxTapped(touch: UITapGestureRecognizer) {
-        let sceneView = touch.view as! SCNView
-        let touchLocation = touch.location(in: sceneView)
-        
-        let touchResults = sceneView.hitTest(touchLocation)
-        
-        guard !touchResults.isEmpty, let node = touchResults.first?.node else { return }
-        let boxMaterial = SCNMaterial()
-        boxMaterial.diffuse.contents = UIColor.blue
-        boxMaterial.specular.contents = UIColor.red
-        
-        node.geometry?.materials[0] = boxMaterial
-    }
-    
-        private func createBox(in scene: SCNScene) {
-    
-            let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-            let boxMaterial = SCNMaterial()
-            
-            boxMaterial.diffuse.contents = UIColor.red
-            boxMaterial.specular.contents = UIColor.yellow
-            
-            let boxNode = SCNNode(geometry: box)
-            boxNode.geometry?.materials = [boxMaterial]
-            boxNode.position = SCNVector3(0.0, 0.0, -1.0)
-            
-            scene.rootNode.addChildNode(boxNode)
+    @IBAction func resetTapped(_ sender: UIButton) {
+        session.pause()
+        sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            if node.name == "box" {
+                node.removeFromParentNode()
+            }
         }
-    
-    private func createDimonRofl(in scene: SCNScene) {
         
-        // Создаем сферу и натягиваем на нее изображение
-        let sphereGeometry = SCNSphere(radius: 0.1)
-        let sphereMaterial = SCNMaterial()
-
-        sphereMaterial.diffuse.contents = UIImage(named: "dima.jpg")
-//        sphereMaterial.diffuse.contentsTransform = SCNMatrix4MakeScale(2, 2, 2) // размер текстуры
-        let sphereNode = SCNNode(geometry: sphereGeometry)
-        sphereNode.scale = SCNVector3(2.0, 2.0, 2.0)
-        sphereNode.geometry?.materials = [sphereMaterial]
-        sphereNode.position = SCNVector3(0, 0, -1)
-        sphereNode.rotation = SCNVector4(x: 0, y: 1, z: 0, w: 3.14)
-
-        scene.rootNode.addChildNode(sphereNode)
-        
-        //Текст
-        let textGeometry = SCNText(string: "eblya", extrusionDepth: 2.0)
-        let textMaterial = SCNMaterial()
-        textMaterial.diffuse.contents = UIColor.red
-        
-        let textNode = SCNNode(geometry: textGeometry)
-        textNode.scale = SCNVector3(0.005, 0.005, 0.005)
-        textNode.geometry?.materials = [textMaterial]
-        
-        textNode.position = SCNVector3(0, 0.2, -1.5)
-        textNode.rotation = SCNVector4(x: 0, y: 1, z: 0, w: 3.14 / 2)
-        scene.rootNode.addChildNode(textNode)
-        
-        
-        // баба
-        let sphereTwoGeometry = SCNSphere(radius: 0.1)
-        let sphereTwoMaterial = SCNMaterial()
-
-        sphereTwoMaterial.diffuse.contents = UIImage(named: "head.jpg")
-//        sphereMaterial.diffuse.contentsTransform = SCNMatrix4MakeScale(2, 2, 2) // размер текстуры
-        let sphereTwoNode = SCNNode(geometry: sphereTwoGeometry)
-        sphereTwoNode.scale = SCNVector3(2.0, 2.0, 2.0)
-        sphereTwoNode.geometry?.materials = [sphereTwoMaterial]
-        sphereTwoNode.position = SCNVector3(0, 0, -2)
-        
-        scene.rootNode.addChildNode(sphereTwoNode)
+        let configuration = ARWorldTrackingConfiguration()
+        session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
     }
-
+    
+    
+    @IBAction func addTapped(_ sender: UIButton) {
+        createBox(in: sceneView.scene)
+    }
+    
+    private func createBox(in scene: SCNScene) {
+        
+        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+        let boxMaterial = SCNMaterial()
+        
+        boxMaterial.diffuse.contents = UIColor.red
+        boxMaterial.specular.contents = UIColor.yellow
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.name = "box"
+        boxNode.geometry?.materials = [boxMaterial]
+        boxNode.position = SCNVector3(0.0, 0.0, -0.5)
+        
+        scene.rootNode.addChildNode(boxNode)
+    }
 }
