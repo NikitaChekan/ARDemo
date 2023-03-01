58dc55bebcf2d3e9b70919afea42abb23f9bddda
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-01 09:26:02 +0300

    created sphere with image

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -22,30 +22,51 @@ class ViewController: UIViewController {
         // Create a new scene
         let scene = SCNScene()
         
-        let boxGeometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
         
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.brown
+        // Создаем куб
+//        let boxGeometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+//
+//        let material = SCNMaterial()
+//        material.diffuse.contents = UIColor.brown
+//
+//        let boxNode = SCNNode(geometry: boxGeometry)
+//        boxNode.geometry?.materials = [material]
+//        boxNode.position = SCNVector3(0, 0, -1.0)
+//
+//        scene.rootNode.addChildNode(boxNode)
         
-        let boxNode = SCNNode(geometry: boxGeometry)
-        boxNode.geometry?.materials = [material]
-        boxNode.position = SCNVector3(0, 0, -1.0)
+        // Создаем текст над кубом
+//        let textGeometry = SCNText(string: "This is the cube", extrusionDepth: 2.0)
+//        let textMaterial = SCNMaterial()
+//        textMaterial.diffuse.contents = UIColor.red
+//
+//        let textNode = SCNNode(geometry: textGeometry)
+//        textNode.scale = SCNVector3(0.005, 0.005, 0.005)
+//        textNode.geometry?.materials = [textMaterial]
+//
+//        textNode.position = SCNVector3(0, 0.2, -1.0)
+//        scene.rootNode.addChildNode(textNode)
         
-        scene.rootNode.addChildNode(boxNode)
+        // Создаем сферу
+        let sphereGeometry = SCNSphere(radius: 0.1)
+        let sphereMaterial = SCNMaterial()
         
-        let textGeometry = SCNText(string: "This is the cube", extrusionDepth: 2.0)
-        let textMaterial = SCNMaterial()
-        textMaterial.diffuse.contents = UIColor.red
-                
-        let textNode = SCNNode(geometry: textGeometry)
-        textNode.scale = SCNVector3(0.005, 0.005, 0.005)
-        textNode.geometry?.materials = [textMaterial]
+        sphereMaterial.diffuse.contents = UIImage(named: "earth.jpg")
+//        sphereMaterial.diffuse.contentsTransform = SCNMatrix4MakeScale(2, 2, 2) // размер текстуры
+        let sphereNode = SCNNode(geometry: sphereGeometry)
+        sphereNode.scale = SCNVector3(2.0, 2.0, 2.0)
+        sphereNode.geometry?.materials = [sphereMaterial]
+        sphereNode.position = SCNVector3(0, 0, -1)
         
-        textNode.position = SCNVector3(0, 0.2, -1.0)
-        scene.rootNode.addChildNode(textNode)
+        scene.rootNode.addChildNode(sphereNode)
         
         sceneView.scene = scene
     }
+    
+//    private func createFigures() {
+//        let array: [SCNGeometry] = [SCNPlane(), SCNSphere(), SCNBox(), SCNPyramid(), SCNTube(), SCNCone(), SCNTorus(), SCNCylinder(), SCNCapsule()]
+//        var xCoordinate: Double
+//    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
